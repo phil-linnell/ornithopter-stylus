569bde8d94b7ae8a02df840401ff9a3a64a2ce8a
Phil Linnell <phil@ustwo.com>
2015-03-27 12:08:30 +0000

    Removed express, completely static, added handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,27 @@
 var gulp = require('gulp');
 
-// var server      = require('gulp-express');
 var stylus      = require('gulp-stylus');
 var nib         = require('nib');
+
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
 var rename      = require('gulp-rename');
-var nodemon     = require('nodemon');
+
+var handlebars 	= require('gulp-compile-handlebars');
+
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
 
 
-gulp.task('nodemon', function () {
-	nodemon({
-	  script: './bin/www'
-  })
-});
-
-
-gulp.task('browser-sync', ['nodemon'], function() {
-    browserSync.init( {
-        proxy: "localhost:10190",
-        port: "10191", // Hey that's the year Dune is set!
-        open: false
-    });
+gulp.task('browser-sync', function() {
+  browserSync.init( {
+		server: {
+			baseDir: "./build"
+		},
+    port: "10191", // Hey that's the year Dune is set!
+    open: false
+  });
 });
 
 
@@ -47,10 +44,27 @@ gulp.task('scripts', function() {
     .pipe(reload({stream:true}));
 });
 
+
+var HBtemplateData = {
+  workingTitle: 'Fremen',
+  title: 'Fremen'
+};
+var HBoptions = {
+  ignorePartials: true,
+  batch: ['./src/views'],
+  helpers: {
+    capitals: function(str){
+      return str.toUpperCase();
+    }
+  }
+};
+
 // Templates
 gulp.task('templates', function() {
-  return gulp.src('views/**/*.hbs')
-    .pipe(reload({stream:true}));
+	return gulp.src('src/views/*.html')
+		.pipe(handlebars(HBtemplateData, HBoptions))
+		.pipe(gulp.dest('build/'))
+		.pipe(reload({stream:true}));
 });
 
 
